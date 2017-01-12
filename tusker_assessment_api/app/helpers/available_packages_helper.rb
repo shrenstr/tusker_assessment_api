9367d018ca888a4ea1bb0893aa9d7e1cf39c0045
shrenitc <shrenstr@gmail.com>
2017-01-12 12:31:11 +0000

    available_packages - retrieve and render

---
+++ tusker_assessment_api/app/helpers/available_packages_helper.rb
@@ -0,0 +1,9 @@
+module AvailablePackagesHelper
+  AVAILABLE_PACKAGES_API="http://tuskermarvel.com/packages.json"
+
+  def get_available_packages()
+    result = Net::HTTP.get(URI.parse(AVAILABLE_PACKAGES_API))
+    binding.pry
+    json = JSON.parse(result)
+  end
+end
---
+++ tusker_assessment_api/spec/controllers/available_packages_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe AvailablePackagesController, type: :controller do
+
+end
